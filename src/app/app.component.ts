8920403f8b047d51f042a09c25592f7ccf983915
hboggavarapu <harshith.boggavarapu@amadeus.com>
2024-08-01 23:59:30 +0530

    Usage of pdf-viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PdfPageCardComponent } from './pdf-page-card/pdf-page-card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PdfPageCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'PDFusion';
+  pdfSrc: string | undefined;
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.pdfSrc = fileReader.result as string;
+    }
+    fileReader.readAsArrayBuffer(file);
+  }
 }
